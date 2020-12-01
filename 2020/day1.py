613d2912ad59dd8e8b5133741cc07ceca3986634
Anirudh Ghantasala <lghantas@purdue.edu>
2020-12-01 11:59:18 -0500

    day 1 completo

---
+++ 2020/day1.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def find_addends(list, sum):
+    for i in list:
+        if (sum-i in list):
+            return sum-i, i
+    return -1, -1
+
+
+nums = np.loadtxt("day1_input.txt", dtype=np.int)
+print(np.prod(find_addends(nums, 2020)))    # part 1
+for i in nums:
+    addends = find_addends(nums, 2020-i)
+    if addends[0] != -1:
+        print(np.prod(addends) * i)         # part 2
+        break
